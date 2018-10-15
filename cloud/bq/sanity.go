06cd630557ddda204162152fee0790d47503edec
yachang <changya@gmail.com>
2018-10-15 11:59:57 -0400

    bqi fix

--- cloud/bq/sanity.go
+++ cloud/bq/sanity.go
@@ -1,3 +1,4 @@
+
 // Package bq provides facilities for sanity checking and
 // copying final table into a destination partition.
 // It is currently somewhat NDT specific:
@@ -383,6 +384,7 @@ func SanityCheckAndCopy(ctx context.Context, src, dest *AnnotatedTable) error {
 	config := bqiface.CopyConfig{}
 	config.WriteDisposition = bigquery.WriteTruncate
 	config.Dst = dest.Table
+        config.Srcs = append(config.Srcs, src.Table)
 	copier.SetCopyConfig(config)
 	log.Println("Copying...", src.TableID())
 	job, err := copier.Run(ctx)
