e15b1c904fc30a2e54abf9029497900b49f690ff
Greg Russell <gfr10598@gmail.com>
2019-04-30 20:39:09 -0400

    fix table detail query

--- cloud/bq/sanity.go
+++ cloud/bq/sanity.go
@@ -168,16 +168,10 @@ func GetTableDetail(ctx context.Context, dsExt *dataset.Dataset, table bqiface.T
 	detail := Detail{}
 	queryString := fmt.Sprintf(`
 		#standardSQL
-        SELECT SUM(tests) AS TestCount, COUNT(DISTINCT task)-1 AS TaskFileCount
-        FROM (
-            -- This avoids null counts when the partition doesn't exist or is empty.
-            SELECT 0 AS tests, "fake-task" AS task
-            UNION ALL
-            SELECT COUNT(DISTINCT test_id) AS tests, task_filename AS task
-            FROM `+"`%s.%s`"+`
-            %s  -- where clause
-            GROUP BY task
-        )`, dataset, tableName, where)
+		SELECT COUNT(DISTINCT test_id) AS TestCount, COUNT(DISTINCT task_filename) AS TaskFileCount
+    FROM `+"`%s.%s`"+`
+    %s  -- where clause
+    GROUP BY task`, dataset, tableName, where)
 
 	// TODO - this should take a context?
 	err := dsExt.QueryAndParse(ctx, queryString, &detail)
