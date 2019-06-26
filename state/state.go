9d75a3e191f3ddd5503db1912e7281dc45f3a05f
Gregory Russell <gfr@google.com>
2019-06-25 22:52:32 -0400

    add TODO unit test

--- state/state.go
+++ state/state.go
@@ -293,6 +293,7 @@ func (t *Task) GetTaskStatus(ctx context.Context) (Task, error) {
 	}
 	status, err := ds.FetchTask(ctx, t.Experiment, t.Name)
 	if err != nil {
+		// TODO - add unit test for this.
 		if err == datastore.ErrNoSuchEntity {
 			return status, nil
 		}
