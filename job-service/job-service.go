83164a61a247207410f608bb0ee075395b07d9dd
Gregory Russell <gfr@google.com>
2019-11-06 15:59:47 -0500

    some cleanup

--- job-service/job-service.go
+++ job-service/job-service.go
@@ -4,25 +4,32 @@ package job
 import "time"
 
 // A Job describes a job dispatched to a Parser.
+// This should be moved to JobTracker package.
 type Job struct {
 	FullPrefix string // The GCS bucket/prefix defining the Job scope.
 
-	LastTaskCompleted string // The complete path to the last task file completed.
-	State             string // String defining the current state - working, completed, failed.
-	Errors            []string
+	ArchivesCompletedThrough string // The complete path to the last task file completed.
+	State                    string // String defining the current state - working, completed, failed.
+	Errors                   []string
 }
 
-// Service contains all information needed to provide a job service.  It iterates through
-// jobs using a
+// Config provides a config for a job service.
+type Config struct {
+	Bucket string
+}
+
+// Service contains all information needed to provide a job service.
+// It iterates through successive dates, processing that date from
+// all TypeSources in the source bucket.
 type Service struct {
-	config Config
+	config Config // Currently just a bucket
 
-	CurrentDate time.Time // The date currently being dispatched.
-	PrefixIndex int       // index of Config.TypePrefixes to dispatch next.
+	CurrentDate   time.Time    // The date currently being dispatched.
+	TypeSources   []TypeSource // The sources to be iterated through.
+	NextTypeIndex int          // index of TypeSource to dispatch next.
 }
 
-// NextJob returns a Job object to dispatch.
-func (svc *Service) NextJob() *Job {
-
-	return nil
+// NextJob returns a prefix to dispatch.
+func (svc *Service) NextJob() string {
+	return ""
 }
--- job-service/misc.go
+++
@@ -1,7 +0,0 @@
-package job
-
-// Config provides a config for a job service.
-type Config struct {
-	Bucket       string
-	TypePrefixes []string // Prefixes of all dates to be handled.
-}
