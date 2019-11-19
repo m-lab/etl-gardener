81978dd21f624e859a15bf11b12d6e4a9d5a0477
Gregory Russell <gfr@google.com>
2019-11-19 17:32:10 -0500

    integration test broken

--- tracker/tracker_integration_test.go
+++ tracker/tracker_integration_test.go
@@ -4,28 +4,50 @@ package tracker_test
 
 import (
 	"context"
+	"log"
 	"testing"
 
+	"github.com/GoogleCloudPlatform/google-cloud-go-testing/datastore/dsiface"
+
 	"cloud.google.com/go/datastore"
-	"github.com/m-lab/etl-gardener/persistence"
 	"github.com/m-lab/etl-gardener/tracker"
 )
 
-// This test assumes the datastore emulator is running.
-func TestWithDatastoreSaver(t *testing.T) {
-	ctx := context.Background()
-	ds, err := persistence.NewDatastoreSaver(ctx, "emulator")
+// This test uses an actual datastore client.
+// It should generally be run with the datastore emulator.
+func TestWithDatastore(t *testing.T) {
+	dsc, err := datastore.NewClient(context.Background(), "mlab-testing")
+	client := dsiface.AdaptClient(dsc)
 	must(t, err)
 
-	o := tracker.NewJobState("foobar")
-	must(t, ds.Save(ctx, &o))
-
-	must(t, ds.Fetch(ctx, &o))
-
-	must(t, ds.Delete(ctx, &o))
-
-	err = ds.Fetch(ctx, &o)
-	if err != datastore.ErrNoSuchEntity {
-		t.Fatal("Should have errored")
+	tk, err := tracker.InitTracker(context.Background(), client, 0)
+	must(t, err)
+	if tk == nil {
+		t.Fatal("nil Tracker")
 	}
+
+	numJobs := 500
+	createJobs(t, tk, "500Jobs", numJobs)
+	if tk.NumJobs() != 500 {
+		t.Fatal("Incorrect number of jobs", tk.NumJobs())
+	}
+
+	log.Println("Calling Sync")
+	must(t, tk.Sync())
+	// Check that the sync (and InitTracker) work.
+	restore, err := tracker.InitTracker(context.Background(), client, 0)
+	must(t, err)
+
+	if restore.NumJobs() != 500 {
+		t.Fatal("Incorrect number of jobs", restore.NumJobs())
+	}
+
+	completeJobs(t, tk, "500Jobs", numJobs)
+
+	tk.Sync()
+
+	if tk.NumJobs() != 0 {
+		t.Error("Job cleanup failed", tk.NumJobs())
+	}
+
 }
