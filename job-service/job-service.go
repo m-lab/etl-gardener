3c82e45608022aa955ba16bd660faea9cc639379
Gregory Russell <gfr@google.com>
2019-12-15 21:28:56 -0500

    simplify job service

--- job-service/job-service.go
+++ job-service/job-service.go
@@ -1,35 +1,82 @@
 // Package job provides an http handler to serve up jobs to ETL parsers.
 package job
 
-import "time"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"regexp"
+	"sync"
+	"time"
 
-// A Job describes a job dispatched to a Parser.
-// This should be moved to JobTracker package.
-type Job struct {
-	FullPrefix string // The GCS bucket/prefix defining the Job scope.
-
-	ArchivesCompletedThrough string // The complete path to the last task file completed.
-	State                    string // String defining the current state - working, completed, failed.
-	Errors                   []string
-}
-
-// Config provides a config for a job service.
-type Config struct {
-	Bucket string
-}
+	"github.com/m-lab/etl-gardener/tracker"
+)
 
 // Service contains all information needed to provide a job service.
 // It iterates through successive dates, processing that date from
 // all TypeSources in the source bucket.
 type Service struct {
-	config Config // Currently just a bucket
+	lock sync.Mutex
 
-	CurrentDate   time.Time    // The date currently being dispatched.
-	TypeSources   []TypeSource // The sources to be iterated through.
-	NextTypeIndex int          // index of TypeSource to dispatch next.
+	startDate  time.Time      // The date to restart at.
+	date       time.Time      // The date currently being dispatched.
+	dateFilter *regexp.Regexp // filter to apply to dates.
+	jobTypes   []tracker.Job  // The job prefixes to be iterated through.
+	nextIndex  int            // index of TypeSource to dispatch next.
 }
 
-// NextJob returns a prefix to dispatch.
-func (svc *Service) NextJob() string {
-	return ""
+func (svc *Service) advanceDate() {
+	date := svc.date.UTC().Add(24 * time.Hour).Truncate(24 * time.Hour)
+	// Skip until we find a date that matches the date filter.
+	for svc.dateFilter != nil && !svc.dateFilter.MatchString(date.Format("2006/01/02")) {
+		date = date.UTC().Add(24 * time.Hour).Truncate(24 * time.Hour)
+	}
+	if time.Since(date) < 36*time.Hour {
+		date = svc.startDate
+	}
+	svc.date = date
+	svc.nextIndex = 0
+}
+
+// NextJob returns a tracker.Job to dispatch.
+func (svc *Service) NextJob() tracker.Job {
+	svc.lock.Lock()
+	defer svc.lock.Unlock()
+	if svc.nextIndex >= len(svc.jobTypes) {
+		svc.advanceDate()
+	}
+	job := svc.jobTypes[svc.nextIndex]
+	svc.nextIndex++
+	return job
+}
+
+// JobHandler handle requests for new jobs.
+func (svc *Service) JobHandler(resp http.ResponseWriter, req *http.Request) {
+	// Must be a post because it changes state.
+	if req.Method != http.MethodPost {
+		resp.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	job := svc.NextJob()
+
+	js, err := json.Marshal(job)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = resp.Write(js)
+	if err != nil {
+		log.Println(err)
+	}
+	resp.WriteHeader(http.StatusOK)
+}
+
+// NewJobService creates the default job service.
+func NewJobService(startDate time.Time) (*Service, error) {
+	types := []tracker.Job{
+		tracker.Job{Bucket: "archive-measurement-lab", Experiment: "ndt", Datatype: "ndt5"},
+		tracker.Job{Bucket: "archive-measurement-lab", Experiment: "ndt", Datatype: "tcpinfo"},
+	}
+
+	return &Service{startDate: startDate, date: startDate, dateFilter: nil, jobTypes: types}, nil
 }
