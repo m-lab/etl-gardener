af5ffb4191cc6f241260fee48c5a6df8fab9dde9
Greg Russell <gfr10598@gmail.com>
2020-01-06 19:36:34 -0500

    remove stuff we dont need yet

--- ops/ops.go
+++ ops/ops.go
@@ -9,15 +9,7 @@ import (
 
 	"github.com/m-lab/go/logx"
 
-	"cloud.google.com/go/bigquery"
-	"github.com/GoogleCloudPlatform/google-cloud-go-testing/bigquery/bqiface"
-
-	"github.com/m-lab/etl/etl"
-	"github.com/m-lab/go/dataset"
-
 	"github.com/m-lab/etl-gardener/cloud"
-	"github.com/m-lab/etl-gardener/cloud/bq"
-	"github.com/m-lab/etl-gardener/state"
 	"github.com/m-lab/etl-gardener/tracker"
 )
 
@@ -108,40 +100,6 @@ func (m *Monitor) Watch(ctx context.Context, tk *tracker.Tracker, period time.Du
 	}
 }
 
-// TemplateTable creates BQ Table for legacy source templated table
-func TemplateTable(j tracker.Job, ds *dataset.Dataset) bqiface.Table {
-	tableName := etl.DirToTablename(j.Datatype)
-
-	src := ds.Table(tableName + "_" + j.Date.Format("20060102"))
-	return src
-}
-
-// This is a function I didn't want to port to the new architecture.  8-(
-func (m *Monitor) waitForStableTable(ctx context.Context, j tracker.Job) error {
-	log.Println("Stabilizing:", j)
-	// Wait for the streaming buffer to be nil.
-
-	// Code snippet adapted from dataset.NewDataset
-	c, err := bigquery.NewClient(ctx, m.bqconfig.BQProject, m.bqconfig.Options...)
-	if err != nil {
-		return err
-	}
-	bqClient := bqiface.AdaptClient(c)
-	ds := dataset.Dataset{Dataset: bqClient.Dataset(m.bqconfig.BQBatchDataset), BqClient: bqClient}
-
-	src := TemplateTable(j, &ds)
-	err = bq.WaitForStableTable(ctx, src)
-	if err != nil {
-		// When testing, we expect to get ErrTableNotFound here.
-		if err != state.ErrTableNotFound {
-			// t.SetError(ctx, err, "bq.WaitForStableTable")
-			return err
-		}
-	}
-
-	return nil
-}
-
 func trueCondition(ctx context.Context, j tracker.Job) bool {
 	return true
 }
@@ -160,20 +118,4 @@ func newStateFunc(state tracker.State) OpFunc {
 func NewMonitor(config cloud.BQConfig) *Monitor {
 	m := Monitor{bqconfig: config, actions: make(map[tracker.State]Action, 10), activeJobs: make(map[tracker.Job]struct{}, 10)}
 	return &m
-}
-
-// StandardMonitor creates the standard monitor that handles several state transitions.
-// It is currently incomplete.
-func StandardMonitor(config cloud.BQConfig) *Monitor {
-	m := NewMonitor(config)
-	m.AddAction(tracker.ParseComplete,
-		trueCondition,
-		newStateFunc(tracker.Stabilizing),
-		"Stabilizing")
-	m.AddAction(tracker.Stabilizing,
-		// HACK
-		func(ctx context.Context, j tracker.Job) bool { return m.waitForStableTable(ctx, j) == nil },
-		newStateFunc(tracker.Deduplicating),
-		"Deduplicating not implemented")
-	return m
 }
