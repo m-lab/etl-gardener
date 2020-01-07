1fe19f84141a455121863efe5c5942d4dbe11603
Greg Russell <gfr10598@gmail.com>
2020-01-07 12:08:58 -0500

    fix race

--- ops/ops.go
+++ ops/ops.go
@@ -13,6 +13,17 @@ import (
 	"github.com/m-lab/etl-gardener/tracker"
 )
 
+/*
+This package defines Action and Monitor.  Actions are applied to jobs in matching States.
+The jJb is "locked" to prevent competing actions, a ConditionFunc is executed to determine
+if the Job satisfies the preconditions, and then the OpFunc is applied, which should
+ultimately update the Job's State.
+
+A Monitor is initialized by adding Actions for each State that should be handled.  The client
+code should then invoke "go Watch(...)" to start watching the tracker for eligible Jobs.
+
+*/
+
 var debug = logx.Debug
 
 // A ConditionFunc checks whether a Job meets some condition.
@@ -21,14 +32,14 @@ type ConditionFunc = func(ctx context.Context, job tracker.Job) bool
 
 // An OpFunc performs an operation on a job, and updates its state.
 // These functions may take a long time to complete, and may be resource intensive.
-type OpFunc = func(ctx context.Context, tr *tracker.Tracker, job tracker.Job)
+type OpFunc = func(ctx context.Context, tr *tracker.Tracker, job tracker.Job, status tracker.Status)
 
 // An Action describes an operation to be applied to jobs that meet the required condition.
 type Action struct {
 	state      tracker.State // State that action applies to.
 	condition  ConditionFunc // Condition that must be satisfied before applying action.
 	op         OpFunc        // Action to apply.
-	annotation string        // Annotation to be used for UpdateDetail while applying Op.
+	annotation string        // Annotation to be used for UpdateDetail while applying Op
 }
 
 // Monitor "owns" all jobs in the states that have actions.
@@ -76,23 +87,24 @@ func (m *Monitor) Watch(ctx context.Context, tk *tracker.Tracker, period time.Du
 
 		case <-ticker.C:
 			debug.Println("================")
+			// These jobs may be deleted by other calls to GetAll, so tk.UpdateJob may fail.
 			jobs := tk.GetAll()
 			for j, s := range jobs {
 				if a, ok := m.actions[s.State]; ok {
 					// If job is not already active.
 					if m.lockJob(j) {
-						go func(j tracker.Job, a Action, unlocker func()) {
+						go func(j tracker.Job, s tracker.Status, a Action, unlocker func()) {
 							defer unlocker()
 							if a.condition(ctx, j) {
-								s.UpdateDetail = a.annotation
-								tk.UpdateJob(j, s)
+								// These jobs may be deleted by other calls to GetAll, so tk.UpdateJob may fail.
+								// s.UpdateDetail = a.annotation
+								// tk.UpdateJob(j, s)
 								// The op should also update the job state, detail, and error.
 								if a.op != nil {
-									a.op(ctx, tk, j)
+									a.op(ctx, tk, j, s)
 								}
-								delete(m.activeJobs, j)
 							}
-						}(j, a, m.unlocker(j))
+						}(j, s, a, m.unlocker(j))
 					}
 				}
 			}
@@ -105,9 +117,9 @@ func trueCondition(ctx context.Context, j tracker.Job) bool {
 }
 
 func newStateFunc(state tracker.State) OpFunc {
-	return func(ctx context.Context, tk *tracker.Tracker, j tracker.Job) {
+	return func(ctx context.Context, tk *tracker.Tracker, j tracker.Job, s tracker.Status) {
 		debug.Println(j, state)
-		err := tk.SetStatus(j, state, "")
+		err := tk.SetStatus(j, state)
 		if err != nil {
 			log.Println(err)
 		}
