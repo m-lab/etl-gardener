ac53b5e98d5a484381097e1a60f070896575d774
Gregory Russell <gfr10598@users.noreply.github.com>
2020-01-10 18:01:21 -0500

    Tracker ops (#225)
    * prototype ops implementation
    
    * remove obsolete skip_cleanup
    
    * empty test framework
    
    * remove stuff we dont need yet
    
    * fix race
    
    * minor tweaks
    
    * add race testing
    
    * improve readability
    
    * move test func
    
    * fix test states and annotations
    
    * add action duration metric

---
+++ ops/ops.go
@@ -0,0 +1,165 @@
+// Package ops provides code that observes the tracker state, and takes appropriate actions.
+// It basically implements a simple state machine.
+package ops
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/m-lab/go/logx"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+
+	"github.com/m-lab/etl-gardener/cloud"
+	"github.com/m-lab/etl-gardener/tracker"
+)
+
+/*
+This package defines Action and Monitor.  Actions are applied to jobs in matching States.
+The Job is "locked" to prevent competing actions, a ConditionFunc is executed to determine
+if the Job satisfies the preconditions, and then the OpFunc is applied, which should
+ultimately update the Job's State.
+
+A Monitor is initialized by adding Actions for each State that should be handled.  The client
+code should then invoke "go Watch(...)" to start watching the tracker for eligible Jobs.
+
+The package assumes that there is only one Action associated with a State, and
+that States with Actions are never operated on independently by some other agent,
+such as the Parser.  Should this cease to be true, then the claim mechanism
+should be moved into the tracker.
+*/
+
+var debug = logx.Debug
+
+var actionDuration = promauto.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Name: "gardener_action_duration",
+		Help: "Histogram of duration of actions",
+		Buckets: []float64{
+			0.1, 0.14, 0.20, 0.27, 0.37, 0.52, 0.72,
+			1, 1.4, 2.0, 2.7, 3.7, 5.2, 7.2,
+			10, 14, 20, 27, 37, 52, 72,
+			100, 140, 200, 270, 370, 520, 720,
+			1000, 1400, 2000, 2700, 3700, 5200, 7200,
+			10000, 14000, 20000, 27000, 37000, 52000, 72000,
+		},
+	},
+	[]string{"action"},
+)
+
+// A ConditionFunc checks whether a Job meets some condition.
+// These functions may take a long time to complete, but should NOT use a lot of resources.
+type ConditionFunc = func(ctx context.Context, job tracker.Job) bool
+
+// An ActionFunc performs an operation on a job, and updates its state.
+// These functions may take a long time to complete, and may be resource intensive.
+type ActionFunc = func(ctx context.Context, tr *tracker.Tracker, job tracker.Job, status tracker.Status)
+
+// An Action describes an operation to be applied to jobs that meet the required condition.
+type Action struct {
+	Name string
+
+	state tracker.State // State that action applies to.
+
+	// condition or action may be nil if not required
+	// condition that must be satisfied before applying action.
+	condition ConditionFunc
+	// action performs an action on the job, and
+	// updates the state when complete.  It may place the job into an error state.
+	action ActionFunc
+
+	annotation string // Annotation to be used for UpdateDetail while applying Op
+}
+
+// Monitor "owns" all jobs in the states that have actions.
+type Monitor struct {
+	bqconfig cloud.BQConfig           // static after creation
+	actions  map[tracker.State]Action // static after creation
+
+	tk *tracker.Tracker
+
+	lock      sync.Mutex               // protects jobClaims
+	jobClaims map[tracker.Job]struct{} // Claimed jobs currently being acted on.
+}
+
+// releaser creates a function that releases the claim on a job.
+func (m *Monitor) releaser(j tracker.Job) func() {
+	return func() {
+		m.lock.Lock()
+		defer m.lock.Unlock()
+		delete(m.jobClaims, j)
+	}
+}
+
+// Returns releaser if successful, nil otherwise.
+func (m *Monitor) tryClaimJob(j tracker.Job) func() {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if _, ok := m.jobClaims[j]; ok {
+		return nil
+	}
+	m.jobClaims[j] = struct{}{}
+	return m.releaser(j)
+}
+
+// AddAction adds a specific action to the Monitor.
+func (m *Monitor) AddAction(name string, state tracker.State, cond ConditionFunc, op ActionFunc, annotation string) {
+	m.actions[state] = Action{name, state, cond, op, annotation}
+}
+
+// applyAction tries to claim a job and apply an action.  Returns false if the job is already claimed.
+func (m *Monitor) tryApplyAction(ctx context.Context, a Action, j tracker.Job, s tracker.Status) bool {
+	// If job is not already claimed.
+	releaser := m.tryClaimJob(j)
+	if releaser == nil {
+		return false
+	}
+	go func(j tracker.Job, s tracker.Status, a Action, releaser func()) {
+		defer releaser()
+		if a.condition == nil || a.condition(ctx, j) {
+			// These jobs may be deleted by other calls to GetAll, so tk.UpdateJob may fail.
+			// s.UpdateDetail = a.annotation
+			// tk.UpdateJob(j, s)
+			// The op should also update the job state, detail, and error.
+			if a.action != nil {
+				start := time.Now()
+				a.action(ctx, m.tk, j, s)
+				actionDuration.WithLabelValues(a.Name).Observe(time.Since(start).Seconds())
+			}
+		}
+	}(j, s, a, releaser)
+	return true
+}
+
+// Watch polls the tracker, and takes appropriate actions.
+func (m *Monitor) Watch(ctx context.Context, period time.Duration) {
+	ticker := time.NewTicker(period)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case <-ticker.C:
+			debug.Println("===== Monitor Loop Starting =====")
+			// These jobs may be deleted by other calls to GetAll, so tk.UpdateJob may fail.
+			jobs := m.tk.GetAll()
+			// Iterate over the job/status map...
+			for j, s := range jobs {
+				// If job is in a state that has an associated action...
+				if a, ok := m.actions[s.State]; ok {
+					m.tryApplyAction(ctx, a, j, s)
+				}
+			}
+		}
+	}
+}
+
+// NewMonitor creates a Monitor with no Actions
+func NewMonitor(config cloud.BQConfig, tk *tracker.Tracker) *Monitor {
+	m := Monitor{bqconfig: config, actions: make(map[tracker.State]Action),
+		tk: tk, jobClaims: make(map[tracker.Job]struct{})}
+	return &m
+}
---
+++ ops/ops_test.go
@@ -0,0 +1,74 @@
+// Package ops provides code that observes the tracker state, and takes appropriate actions.
+package ops_test
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/m-lab/go/logx"
+
+	"github.com/m-lab/etl-gardener/cloud"
+	"github.com/m-lab/etl-gardener/ops"
+	"github.com/m-lab/etl-gardener/tracker"
+	"github.com/m-lab/go/rtx"
+)
+
+func init() {
+	// Always prepend the filename and line number.
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func newStateFunc(state tracker.State) ops.ActionFunc {
+	return func(ctx context.Context, tk *tracker.Tracker, j tracker.Job, s tracker.Status) {
+		log.Println(j, state)
+		err := tk.SetStatus(j, state)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func TestMonitor_Watch(t *testing.T) {
+	logx.LogxDebug.Set("true")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	tk, err := tracker.InitTracker(ctx, nil, nil, 0)
+	rtx.Must(err, "tk init")
+	tk.AddJob(tracker.NewJob("bucket", "exp", "type", time.Now()))
+	tk.AddJob(tracker.NewJob("bucket", "exp2", "type", time.Now()))
+	tk.AddJob(tracker.NewJob("bucket", "exp2", "type2", time.Now()))
+
+	m := ops.NewMonitor(cloud.BQConfig{}, tk)
+	m.AddAction("Init", tracker.Init,
+		nil,
+		newStateFunc(tracker.Parsing),
+		"Init")
+	m.AddAction("Parsing", tracker.Parsing,
+		nil,
+		newStateFunc(tracker.ParseComplete),
+		"Parsing")
+	m.AddAction("ParseComplete", tracker.ParseComplete,
+		nil,
+		newStateFunc(tracker.Stabilizing),
+		"PostProcessing")
+	m.AddAction("Stabilizing", tracker.Stabilizing,
+		nil,
+		newStateFunc(tracker.Deduplicating),
+		"Checking for stability")
+	m.AddAction("Deduplicating", tracker.Deduplicating,
+		nil,
+		newStateFunc(tracker.Complete),
+		"Deduplicating")
+	go m.Watch(ctx, 10*time.Millisecond)
+
+	failTime := time.Now().Add(10 * time.Second)
+
+	for time.Now().Before(failTime) && tk.NumJobs() > 0 {
+	}
+	if tk.NumJobs() != 0 {
+		t.Error(tk.NumJobs())
+	}
+	cancel()
+}
