12e5fc5f2013e860d6889b1fa0b08e79b8f525cf
Gregory Russell <gfr@google.com>
2020-03-24 15:20:18 -0400

    Add Dedup func

--- dedup/dedup.go
+++ dedup/dedup.go
@@ -3,28 +3,35 @@ package dedup
 
 import (
 	"bytes"
+	"context"
 	"html/template"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/googleapis/google-cloud-go-testing/bigquery/bqiface"
+
+	"github.com/m-lab/go/dataset"
 
 	"github.com/m-lab/etl-gardener/tracker"
 )
 
 // TODO remove _tmp when we are ready to transition.
-const table = "`{{.Project}}.{{.Job.Experiment}}_tmp.{{.Job.Datatype}}`"
+const table = "`{{.Project}}.{{.Job.Experiment}}_raw.{{.Job.Datatype}}`"
 
 var dedupTemplate = template.Must(template.New("").Parse(`
 #standardSQL
 # Delete all duplicate rows based on key and prefered priority ordering.
 # This is resource intensive for tcpinfo - 20 slot hours for 12M rows with 250M snapshots,
 # roughly proportional to the memory footprint of the table partition.
+# The query is very cheap if there are no duplicates.
 DELETE
 FROM ` + table + ` AS target
   WHERE Date(TestTime) = "{{.Job.Date.Format "2006-01-02"}}"
   # This identifies all rows that don't match rows to preserve.
   AND NOT EXISTS (
-	# This creates list of rows to preserve, based on key and priority.
+    # This creates list of rows to preserve, based on key and priority.
     WITH keep AS (
     SELECT * EXCEPT(row_number) FROM (
-      SELECT {{.Key}}, ParseInfo.ParseTime
+      SELECT {{.Key}}, ParseInfo.ParseTime,
         ROW_NUMBER() OVER (PARTITION BY {{.Key}} ORDER BY {{.Order}}) row_number
         FROM (
           SELECT * FROM ` + table + `
@@ -33,11 +40,11 @@ FROM ` + table + ` AS target
       )
       WHERE row_number = 1
     )
-	SELECT * FROM keep
-	# This matches against the keep table based on key and parsetime, so it should retain
-	# only the rows that were selected in the keep table.  Parsetime is used in lieu of
-	# any other distinguishing traits that create different row numbers in the keep query.
-	# Without the parsetime, it keeps all the rows.
+    SELECT * FROM keep
+    # This matches against the keep table based on key and parsetime, so it should retain
+    # only the rows that were selected in the keep table.  Parsetime is used in lieu of
+    # any other distinguishing traits that create different row numbers in the keep query.
+    # Without the parsetime, it keeps all the rows.
     WHERE target.{{.Key}} = keep.{{.Key}} AND target.ParseInfo.ParseTime = keep.ParseTime
   )`))
 
@@ -62,4 +69,21 @@ func tcpinfoQuery(job tracker.Job, project string) string {
 		Key:     "uuid",
 		Order:   "ARRAY_LENGTH(Snapshots) DESC, ParseInfo.TaskFileName, ParseInfo.ParseTime DESC",
 	}.String()
+}
+
+// Dedup executes a query that deletes duplicates from the destination table.
+// It derives the table name from the dsExt project and the job fields.
+// TODO add cost accounting to status page?
+// TODO inject fake bqclient for testing?
+func (params QueryParams) Dedup(ctx context.Context) (bqiface.Job, error) {
+	c, err := bigquery.NewClient(ctx, params.Project)
+	if err != nil {
+		return nil, err
+	}
+	bqClient := bqiface.AdaptClient(c)
+	q := bqClient.Query(params.String())
+	if q == nil {
+		return nil, dataset.ErrNilQuery
+	}
+	return q.Run(ctx)
 }
--- dedup/dedup_test.go
+++ dedup/dedup_test.go
@@ -1,6 +1,7 @@
 package dedup
 
 import (
+	"context"
 	"strings"
 	"testing"
 	"time"
@@ -20,4 +21,25 @@ func TestTemplate(t *testing.T) {
 	if !strings.Contains(q, "ParseInfo.TaskFileName") {
 		t.Error("query should contain ParseInfo.TaskFileName:\n", q)
 	}
+}
+
+// This runs a real dedup on a real partition in mlab-sandbox for manual testing.
+// It takes about 5 minutes to run.
+func xTestDedup(t *testing.T) {
+	job := tracker.NewJob("foobar", "ndt", "tcpinfo", time.Date(2019, 8, 12, 0, 0, 0, 0, time.UTC))
+	qp := QueryParams{
+		Project: "mlab-sandbox",
+		Job:     job,
+		Key:     "uuid",
+		Order:   "ARRAY_LENGTH(Snapshots) DESC, ParseInfo.TaskFileName, ParseInfo.ParseTime DESC",
+	}
+	bqjob, err := qp.Dedup(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	status, err := bqjob.Wait(context.Background())
+	if err != nil {
+		t.Fatal(err, qp.String())
+	}
+	t.Error(status)
 }
