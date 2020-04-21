53e9ec9e2b95a6b34943ffd34429d57a21514e3c
Gregory Russell <gfr10598@users.noreply.github.com>
2020-04-21 16:18:50 -0400

    Bq ops (#262)
    * move dedup to cloud/bq
    
    * refactor and add datatypes
    
    * cleanup for PR feedback
    
    * hard code ParseInfo.ParseTime
    
    * use fake-project

---
+++ cloud/bq/ops.go
@@ -0,0 +1,214 @@
+package bq
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"html/template"
+	"log"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/googleapis/google-cloud-go-testing/bigquery/bqiface"
+
+	"github.com/m-lab/go/dataset"
+
+	"github.com/m-lab/etl-gardener/tracker"
+)
+
+// Queryer provides the interface for running bigquery operations.
+type Queryer interface {
+	QueryFor(key string) string
+	Run(ctx context.Context, key string, dryRun bool) (bqiface.Job, error)
+	Copy(ctx context.Context, dryRun bool) (bqiface.Job, error)
+}
+
+// queryer is used to construct a dedup query.
+type queryer struct {
+	client   bqiface.Client
+	Project  string
+	TestTime string // Name of the partition field
+	Job      tracker.Job
+	// map key is the single field name, value is fully qualified name
+	Partition map[string]string
+	Order     string
+}
+
+// ErrDatatypeNotSupported is returned by Query for unsupported datatypes.
+var ErrDatatypeNotSupported = errors.New("Datatype not supported")
+
+// NewQueryParams creates a suitable QueryParams for a Job.
+func NewQueryParams(job tracker.Job, project string) (Queryer, error) {
+	c, err := bigquery.NewClient(context.Background(), project)
+	if err != nil {
+		return nil, err
+	}
+	bqClient := bqiface.AdaptClient(c)
+	return NewQueryParamsWithClient(bqClient, job, project)
+}
+
+// NewQueryParamsWithClient creates a suitable QueryParams for a Job.
+func NewQueryParamsWithClient(client bqiface.Client, job tracker.Job, project string) (Queryer, error) {
+	switch job.Datatype {
+	case "annotation":
+		return &queryer{
+			client:    client,
+			Project:   project,
+			TestTime:  "TestTime",
+			Job:       job,
+			Partition: map[string]string{"UUID": "UUID"},
+			Order:     "",
+		}, nil
+
+	case "ndt5":
+		return &queryer{
+			client:    client,
+			Project:   project,
+			TestTime:  "log_time",
+			Job:       job,
+			Partition: map[string]string{"test_id": "test_id"},
+			Order:     "",
+		}, nil
+
+	case "ndt7":
+		return &queryer{
+			client:    client,
+			Project:   project,
+			TestTime:  "TestTime",
+			Job:       job,
+			Partition: map[string]string{"UUID": "a.UUID"},
+			Order:     "",
+		}, nil
+
+	case "tcpinfo":
+		return &queryer{
+			client:    client,
+			Project:   project,
+			TestTime:  "TestTime",
+			Job:       job,
+			Partition: map[string]string{"uuid": "uuid", "Timestamp": "FinalSnapshot.Timestamp"},
+			Order:     "ARRAY_LENGTH(Snapshots) DESC, ParseInfo.TaskFileName, ",
+		}, nil
+	default:
+		return nil, ErrDatatypeNotSupported
+	}
+}
+
+var queryTemplates = map[string]*template.Template{
+	"dedup":   dedupTemplate,
+	"cleanup": cleanupTemplate,
+}
+
+// MakeQuery creates a query from a template.
+func (params queryer) makeQuery(t *template.Template) string {
+	out := bytes.NewBuffer(nil)
+	err := t.Execute(out, params)
+	if err != nil {
+		log.Println(err)
+	}
+	return out.String()
+}
+
+// QueryFor returns the appropriate query in string form.
+func (params queryer) QueryFor(key string) string {
+	t, ok := queryTemplates[key]
+	if !ok {
+		return ""
+	}
+	return params.makeQuery(t)
+}
+
+// Run executes a query constructed from a template.  It returns the bqiface.Job.
+func (params queryer) Run(ctx context.Context, key string, dryRun bool) (bqiface.Job, error) {
+	qs := params.QueryFor(key)
+	if len(qs) == 0 {
+		return nil, dataset.ErrNilQuery
+	}
+	if params.client == nil {
+		return nil, dataset.ErrNilBqClient
+	}
+	q := params.client.Query(qs)
+	if q == nil {
+		return nil, dataset.ErrNilQuery
+	}
+	if dryRun {
+		qc := bqiface.QueryConfig{QueryConfig: bigquery.QueryConfig{DryRun: dryRun, Q: qs}}
+		q.SetQueryConfig(qc)
+	}
+	return q.Run(ctx)
+}
+
+// Copy copies the tmp_ job partition to the raw_ job partition.
+func (params queryer) Copy(ctx context.Context, dryRun bool) (bqiface.Job, error) {
+	if params.client == nil {
+		return nil, dataset.ErrNilBqClient
+	}
+	src := params.client.Dataset("tmp_" + params.Job.Experiment).Table(params.Job.Datatype)
+	dest := params.client.Dataset("raw_" + params.Job.Experiment).Table(params.Job.Datatype)
+
+	copier := dest.CopierFrom(src)
+	config := bqiface.CopyConfig{}
+	config.WriteDisposition = bigquery.WriteTruncate
+	config.Dst = dest
+	config.Srcs = append(config.Srcs, src)
+	copier.SetCopyConfig(config)
+	return copier.Run(ctx)
+}
+
+// Dedup executes a query that deletes duplicates from the destination table.
+func (params queryer) Dedup(ctx context.Context, dryRun bool) (bqiface.Job, error) {
+	return params.Run(ctx, "dedup", dryRun)
+}
+
+// Cleanup executes a query that deletes the entire partition
+// from the tmp table.
+func (params queryer) Cleanup(ctx context.Context, dryRun bool) (bqiface.Job, error) {
+	return params.Run(ctx, "cleanup", dryRun)
+}
+
+// TODO get the tmp_ and raw_ from the job Target?
+const tmpTable = "`{{.Project}}.tmp_{{.Job.Experiment}}.{{.Job.Datatype}}`"
+const rawTable = "`{{.Project}}.raw_{{.Job.Experiment}}.{{.Job.Datatype}}`"
+
+var dedupTemplate = template.Must(template.New("").Parse(`
+#standardSQL
+# Delete all duplicate rows based on key and prefered priority ordering.
+# This is resource intensive for tcpinfo - 20 slot hours for 12M rows with 250M snapshots,
+# roughly proportional to the memory footprint of the table partition.
+# The query is very cheap if there are no duplicates.
+DELETE
+FROM ` + tmpTable + ` AS target
+WHERE Date({{.TestTime}}) = "{{.Job.Date.Format "2006-01-02"}}"
+# This identifies all rows that don't match rows to preserve.
+AND NOT EXISTS (
+  # This creates list of rows to preserve, based on key and priority.
+  WITH keep AS (
+  SELECT * EXCEPT(row_number) FROM (
+    SELECT
+      {{range $k, $v := .Partition}}{{$v}}, {{end}}
+	  ParseInfo.ParseTime,
+      ROW_NUMBER() OVER (
+        PARTITION BY {{range $k, $v := .Partition}}{{$v}}, {{end}}ParseInfo.ParseTime
+        ORDER BY {{.Order}} ParseInfo.ParseTime DESC
+      ) row_number
+      FROM (
+        SELECT * FROM ` + tmpTable + `
+        WHERE Date({{.TestTime}}) = "{{.Job.Date.Format "2006-01-02"}}"
+      )
+    )
+    WHERE row_number = 1
+  )
+  SELECT * FROM keep
+  # This matches against the keep table based on keys.  Sufficient select keys must be
+  # used to distinguish the preferred row from the others.
+  WHERE
+    {{range $k, $v := .Partition}}target.{{$v}} = keep.{{$k}} AND {{end}}
+    target.ParseInfo.ParseTime = keep.ParseTime
+)`))
+
+var cleanupTemplate = template.Must(template.New("").Parse(`
+#standardSQL
+# Delete all rows in a partition.
+DELETE
+FROM ` + tmpTable + `
+WHERE Date({{.TestTime}}) = "{{.Job.Date.Format "2006-01-02"}}"
+`))
---
+++ cloud/bq/ops_test.go
@@ -0,0 +1,75 @@
+package bq_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/m-lab/etl-gardener/cloud/bq"
+	"github.com/m-lab/etl-gardener/tracker"
+	"github.com/m-lab/go/rtx"
+)
+
+func TestTemplate(t *testing.T) {
+	job := tracker.NewJob("bucket", "ndt", "tcpinfo", time.Date(2019, 3, 4, 0, 0, 0, 0, time.UTC))
+	q, err := bq.NewQueryParams(job, "fake-project")
+	rtx.Must(err, "dedup.Query failed")
+	qs := q.QueryFor("dedup")
+	if !strings.Contains(qs, "uuid") {
+		t.Error("query should contain keep.uuid:\n", q)
+	}
+	if !strings.Contains(qs, `"2019-03-04"`) {
+		t.Error(`query should contain "2019-03-04":\n`, q)
+	}
+	if !strings.Contains(qs, "ParseInfo.TaskFileName") {
+		t.Error("query should contain ParseInfo.TaskFileName:\n", q)
+	}
+	// TODO check final WHERE clause.
+	if !strings.Contains(qs, "target.ParseInfo.ParseTime = keep.ParseTime") {
+		t.Error("query should contain target.ParseInfo.ParseTime = ... :\n", qs)
+	}
+}
+
+// NOTE: This validates queries against actual tables in mlab-testing.  It only
+// runs Dryrun queries, so it does not modify the tables.
+func TestValidateQueries(t *testing.T) {
+	if testing.Short() {
+		t.Log("Skipping test for --short")
+	}
+	ctx := context.Background()
+	dataTypes := []string{"tcpinfo", "ndt5", "annotation", "ndt7"}
+	keys := []string{"dedup", "cleanup"} // TODO Add "preserve" query
+	// Test for each datatype
+	for _, dataType := range dataTypes {
+		job := tracker.NewJob("bucket", "ndt", dataType, time.Date(2019, 3, 4, 0, 0, 0, 0, time.UTC))
+		qp, err := bq.NewQueryParams(job, "mlab-testing")
+		if err != nil {
+			t.Fatal(dataType, err)
+		}
+		// Test each query key
+		for _, key := range keys {
+			t.Run(dataType+":"+key, func(t *testing.T) {
+				t.Log(t.Name())
+				j, err := qp.Run(ctx, key, true)
+				if err != nil {
+					t.Fatal(t.Name(), err, qp.QueryFor(key))
+				}
+				status := j.LastStatus()
+				if status.Err() != nil {
+					t.Fatal(t.Name(), err, qp.QueryFor(key))
+				}
+			})
+		}
+		t.Run(dataType+":copy", func(t *testing.T) {
+			j, err := qp.Copy(ctx, true)
+			if err != nil {
+				t.Fatal(t.Name(), err)
+			}
+			status := j.LastStatus()
+			if status.Err() != nil {
+				t.Fatal(t.Name(), err)
+			}
+		})
+	}
+}
