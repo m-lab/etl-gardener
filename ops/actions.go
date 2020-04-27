13f59129002a9798b8fab083ad032a7b403cd155
Gregory Russell <gfr@google.com>
2020-04-27 14:55:23 -0400

    add copy

--- ops/actions.go
+++ ops/actions.go
@@ -49,6 +49,10 @@ func NewStandardMonitor(ctx context.Context, config cloud.BQConfig, tk *tracker.
 		nil,
 		dedupFunc,
 		"Deduplicating")
+	m.AddAction(tracker.Copying,
+		nil,
+		copyFunc,
+		"Copying")
 	return m, nil
 }
 
@@ -137,6 +141,57 @@ func dedupFunc(ctx context.Context, tk *tracker.Tracker, j tracker.Job, s tracke
 			details.TotalBytesProcessed/1000000, details.TotalBytesBilled/1000000)
 		log.Println(msg)
 		log.Printf("Dedup %s: %+v\n", j, details)
+	default:
+		log.Printf("Could not convert to QueryStatistics: %+v\n", status.Statistics.Details)
+		msg = "Could not convert Detail to QueryStatistics"
+	}
+	metrics.StateDate.WithLabelValues(j.Experiment, j.Datatype, string(tracker.Complete)).Set(float64(j.Date.Unix()))
+	err = tk.SetStatus(j, tracker.Copying, msg)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// TODO figure out how to test this code?
+func copyFunc(ctx context.Context, tk *tracker.Tracker, j tracker.Job, s tracker.Status) {
+	start := time.Now()
+	// This is the delay since entering the dedup state, due to monitor delay
+	// and retries.
+	delay := time.Since(s.LastStateChangeTime()).Round(time.Minute)
+
+	var bqJob bqiface.Job
+	var msg string
+	// TODO pass in the JobWithTarget, and get the base from the target.
+	qp, err := bq.NewQuerier(j, os.Getenv("PROJECT"))
+	if err != nil {
+		log.Println(err)
+		// This terminates this job.
+		tk.SetJobError(j, err.Error())
+		return
+	}
+	bqJob, err = qp.Run(ctx, "copy", false)
+	if err != nil {
+		log.Println(err)
+		// Try again soon.
+		return
+	}
+	status := waitAndCheck(ctx, tk, bqJob, j, s, "Copy")
+
+	if status == nil {
+		return
+	}
+
+	// Dedup job was successful.  Handle the statistics, metrics, tracker update.
+	switch details := status.Statistics.Details.(type) {
+	case *bigquery.QueryStatistics:
+		metrics.QueryCostHistogram.WithLabelValues(j.Datatype, "dedup").Observe(float64(details.SlotMillis) / 1000.0)
+		msg = fmt.Sprintf("Copy took %s (after %s waiting), %5.2f Slot Minutes, %d Rows affected, %d MB Processed, %d MB Billed",
+			time.Since(start).Round(100*time.Millisecond).String(),
+			delay,
+			float64(details.SlotMillis)/60000, details.NumDMLAffectedRows,
+			details.TotalBytesProcessed/1000000, details.TotalBytesBilled/1000000)
+		log.Println(msg)
+		log.Printf("Copy %s: %+v\n", j, details)
 	default:
 		log.Printf("Could not convert to QueryStatistics: %+v\n", status.Statistics.Details)
 		msg = "Could not convert Detail to QueryStatistics"
