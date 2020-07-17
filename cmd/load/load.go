449ac4e80c248196740e8a3dbf1905173bc90ae2
Gregory Russell <gfr@google.com>
2020-07-17 16:37:56 -0400

    Add Usage for load CLI

--- cmd/load/load.go
+++ cmd/load/load.go
@@ -13,15 +13,31 @@ import (
 	"github.com/m-lab/go/rtx"
 )
 
+var (
+	datatype = flag.String("datatype", "ndt7", "data type")
+	date     = flag.String("date", "", "date")
+)
+
+var usageText = `
+NAME
+  load - load from a GCS prefix into tmp_ndt.*
+  
+DESCRIPTION
+  loads data for a single datatype/date from json files in GCS into mlab-sandbox.tmp_ndt.datatype table.
+  Intended primarily for testing and development.
+
+EXAMPLES
+  load -datatype=ndt7 -date=2020-03-01
+`
+
 func init() {
 	// Always prepend the filename and line number.
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), usageText)
+		flag.PrintDefaults()
+	}
 }
-
-var (
-	exp  = flag.String("exp", "ndt7", "experiment")
-	date = flag.String("date", "2020/05/22", "date")
-)
 
 func main() {
 	ctx := context.Background()
@@ -33,7 +49,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	j := tracker.NewJob("bucket", "ndt", *exp, d)
+	j := tracker.NewJob("bucket", "ndt", *datatype, d)
 	log.Println(j)
 
 	q, err := bq.NewTableOps(ctx, j, "mlab-sandbox",
