b62803069ed3b1ccc5e9245e7287a6adfd01998a
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-02-27 20:13:40 -0500

    Remove unnecessary variables

--- apply-cluster.sh
+++ apply-cluster.sh
@@ -27,8 +27,6 @@ kubectl create configmap gardener-config --dry-run \
 CFG=/tmp/${CLUSTER}-${PROJECT}.yml
 kexpand expand --ignore-missing-keys k8s/${CLUSTER}/*/*.yml \
     --value GCLOUD_PROJECT=${PROJECT} \
-    --value GIT_COMMIT=${TRAVIS_COMMIT} \
-    --value GIT_TAG=${TRAVIS_TAG} \
     --value DATE_SKIP=${DATE_SKIP} \
     --value TASK_FILE_SKIP=${TASK_FILE_SKIP} \
     > ${CFG}
