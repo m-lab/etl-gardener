b080455648c2b445148a55ef289ace7184c55098
Cristina Leon <cristinaleonr@users.noreply.github.com>
2021-07-30 13:44:54 -0400

    Add gardener configuration for PCAP datatype (#335)
    * Add gardener configuration for ndt/pcap
    
    * Adding unit tests
    
    * Make unit tests more readable

--- cloud/bq/ops_test.go
+++ cloud/bq/ops_test.go
@@ -37,7 +37,7 @@ func TestValidateQueries(t *testing.T) {
 		t.Log("Skipping test for --short")
 	}
 	ctx := context.Background()
-	dataTypes := []string{"annotation", "ndt7"}
+	dataTypes := []string{"annotation", "ndt7", "pcap"}
 	// TODO Add "preserve" query
 	// Test for each datatype
 	for _, dataType := range dataTypes {
@@ -57,15 +57,17 @@ func TestValidateQueries(t *testing.T) {
 				t.Fatal(t.Name(), err, bq.DedupQuery(*qp))
 			}
 
-			if qp.Job.Datatype != "annotation" {
-				j, err := qp.Join(ctx, true)
-				if err != nil {
-					t.Fatal(t.Name(), err, bq.JoinQuery(*qp))
-				}
-				status := j.LastStatus()
-				if status.Err() != nil {
-					t.Fatal(t.Name(), err, bq.JoinQuery(*qp))
-				}
+			if qp.Job.Datatype != "ndt7" {
+				return
+			}
+
+			j, err = qp.Join(ctx, true)
+			if err != nil {
+				t.Fatal(t.Name(), err, bq.JoinQuery(*qp))
+			}
+			status = j.LastStatus()
+			if status.Err() != nil {
+				t.Fatal(t.Name(), err, bq.JoinQuery(*qp))
 			}
 		})
 	}
