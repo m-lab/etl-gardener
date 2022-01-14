aad491a637c9c56ec019e867b6b734628ffeb785
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-01-14 08:41:20 -0500

    Remove TestJobHandler

--- job-service/job-service_test.go
+++ job-service/job-service_test.go
@@ -8,14 +8,12 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
-	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
 	"bou.ke/monkey"
 	"cloud.google.com/go/datastore"
-	"cloud.google.com/go/storage"
 	"github.com/go-test/deep"
 
 	"github.com/m-lab/go/rtx"
@@ -24,8 +22,6 @@ import (
 	"github.com/m-lab/etl-gardener/job-service"
 	"github.com/m-lab/etl-gardener/persistence"
 	"github.com/m-lab/etl-gardener/tracker"
-
-	"github.com/m-lab/go/cloudtest/gcsfake"
 )
 
 func init() {
@@ -105,61 +101,6 @@ func TestService_NextJob(t *testing.T) {
 		if diff != nil {
 			t.Error(i, diff)
 		}
-	}
-}
-
-func TestJobHandler(t *testing.T) {
-	fc := gcsfake.GCSClient{}
-	fc.AddTestBucket("fake-bucket",
-		&gcsfake.BucketHandle{
-			ObjAttrs: []*storage.ObjectAttrs{
-				{Name: "obj1", Updated: time.Now()},
-				{Name: "obj2", Updated: time.Now()},
-				{Name: "ndt/ndt5/2011/02/03/foobar.tgz", Size: 101, Updated: time.Now()},
-			}})
-
-	ctx := context.Background()
-
-	// Fake time will avoid yesterday trigger.
-	now := time.Date(2011, 2, 16, 1, 2, 3, 4, time.UTC)
-	monkey.Patch(time.Now, func() time.Time {
-		return now
-	})
-	defer monkey.Unpatch(time.Now)
-
-	sources := []config.SourceConfig{
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo", Target: "tmp_ndt.tcpinfo"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5", Target: "tmp_ndt.ndt5"},
-	}
-	start := time.Date(2011, 2, 3, 0, 0, 0, 0, time.UTC)
-	svc, err := job.NewJobService(ctx, &NullTracker{}, start, "fake-bucket", sources, &NullSaver{}, &fc)
-	must(t, err)
-	req := httptest.NewRequest("", "/job", nil)
-	resp := httptest.NewRecorder()
-	svc.JobHandler(resp, req)
-	if resp.Code != http.StatusMethodNotAllowed {
-		t.Error("Should be MethodNotAllowed", http.StatusText(resp.Code))
-	}
-
-	// This one should fail because there are no objects with tcpinfo prefix.
-	req = httptest.NewRequest("POST", "/job", nil)
-	resp = httptest.NewRecorder()
-	svc.JobHandler(resp, req)
-	if resp.Code != http.StatusInternalServerError {
-		t.Error("Should be InternalServerError", http.StatusText(resp.Code), resp.Body.String())
-	}
-
-	// This should succeed, because the service advanced to ndt5/2011/02/03/
-	req = httptest.NewRequest("POST", "/job", nil)
-	resp = httptest.NewRecorder()
-	svc.JobHandler(resp, req)
-	if resp.Code != http.StatusOK {
-		t.Error("Should be StatusOK", http.StatusText(resp.Code), resp.Body.String())
-	}
-
-	want := `{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-03T00:00:00Z"}`
-	if want != resp.Body.String() {
-		t.Fatal(resp.Body.String())
 	}
 }
 
