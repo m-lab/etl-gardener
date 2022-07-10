cec51dd091bce83aa0f43e9846fe0000a3ec909d
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-07-10 10:09:50 -0400

    Identify join types from config Datasets

--- ops/actions_test.go
+++ ops/actions_test.go
@@ -1,3 +1,4 @@
+//go:build integration
 // +build integration
 
 package ops_test
@@ -9,6 +10,7 @@ import (
 	"time"
 
 	"github.com/m-lab/etl-gardener/cloud"
+	"github.com/m-lab/etl-gardener/config"
 	"github.com/m-lab/etl-gardener/ops"
 	"github.com/m-lab/etl-gardener/tracker"
 	"github.com/m-lab/go/logx"
@@ -23,17 +25,71 @@ func TestStandardMonitor(t *testing.T) {
 	}
 	logx.LogxDebug.Set("true")
 
+	d := time.Now()
+	// Statically define jobs with full configurations specified.
+	jobs := []tracker.Job{
+		{
+			Bucket:     "bucket",
+			Experiment: "exp",
+			Datatype:   "type",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_exp", Raw: "raw_exp"},
+		},
+		// Not yet supported.
+		{
+			Bucket:     "bucket",
+			Experiment: "exp2",
+			Datatype:   "tcpinfo",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_exp2", Raw: "raw_exp2"},
+		},
+		// Valid experiment and datatype
+		// This does an actual dedup, so we need to allow enough time.
+		{
+			Bucket:     "bucket",
+			Experiment: "ndt",
+			Datatype:   "ndt7",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_ndt", Raw: "raw_ndt", Join: "ndt"},
+		},
+		{
+			Bucket:     "bucket",
+			Experiment: "ndt",
+			Datatype:   "annotation",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_ndt", Raw: "raw_ndt"},
+		},
+		{
+			Bucket:     "bucket",
+			Experiment: "ndt",
+			Datatype:   "pcap",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_ndt", Raw: "raw_ndt"},
+		},
+		{
+			Bucket:     "bucket",
+			Experiment: "ndt",
+			Datatype:   "hopannotation1",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_ndt", Raw: "raw_ndt"},
+		},
+		{
+			Bucket:     "bucket",
+			Experiment: "ndt",
+			Datatype:   "scamper1",
+			Date:       d,
+			Datasets:   config.Datasets{Temp: "tmp_ndt", Raw: "raw_ndt", Join: "ndt"},
+		},
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	saver := tracker.NewLocalSaver(t.TempDir(), nil)
 	tk, err := tracker.InitTracker(ctx, saver, 0, 0, 0)
 	rtx.Must(err, "tk init")
-	// Not supported.
-	tk.AddJob(tracker.NewJob("bucket", "exp", "type", time.Now()))
-	// Valid experiment and datatype
-	// This does an actual dedup, so we need to allow enough time.
-	datatypes := []string{"ndt7", "annotation", "pcap", "hopannotation1", "scamper1", "tcpinfo"}
-	for _, datatype := range datatypes {
-		tk.AddJob(tracker.NewJob("bucket", "ndt", datatype, time.Now()))
+
+	// Add jobs to the tracker.
+	for i := 0; i < len(jobs); i++ {
+		tk.AddJob(jobs[i])
 	}
 
 	m, err := ops.NewStandardMonitor(context.Background(), "mlab-testing", cloud.BQConfig{}, tk)
