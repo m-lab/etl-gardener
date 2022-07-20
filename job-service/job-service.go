e000575679f3e8981971bcce9e31dbce87235dad
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-07-20 07:36:46 -0400

    Use date iterators for job service

--- job-service/job-service.go
+++ job-service/job-service.go
@@ -5,125 +5,26 @@ import (
 	"context"
 	"errors"
 	"log"
-	"reflect"
 	"sync"
 	"time"
-
-	"github.com/m-lab/go/timex"
 
 	"github.com/googleapis/google-cloud-go-testing/storage/stiface"
 
 	"github.com/m-lab/etl-gardener/config"
-	"github.com/m-lab/etl-gardener/persistence"
 	"github.com/m-lab/etl-gardener/tracker"
 )
 
 // ErrMoreJSON is returned when response from gardener has unknown fields.
 var ErrMoreJSON = errors.New("JSON body not completely consumed")
 
-// ErrNilParameter is returned on disallowed nil parameter.
-var ErrNilParameter = errors.New("nil parameter not allowed")
-
-type jobAdder interface {
-	AddJob(job tracker.Job) error
-	LastJob() tracker.Job // temporary
-}
-
-// YesterdaySource provides pending jobs for yesterday's data.
-// When the scheduled delay has passed, it cycles through all the
-// job specs, then advances to the next date.
-type YesterdaySource struct {
-	saver persistence.Saver
-
-	jobSpecs  []tracker.JobWithTarget // The job prefixes to be iterated through.
-	Date      time.Time               // The next "yesterday" date to be processed.
-	delay     time.Duration           // time after UTC to process yesterday.
-	nextIndex int
-}
-
-// nextJob returns a yesterday Job if appropriate
-// Not thread-safe.
-func (y *YesterdaySource) nextJob(ctx context.Context) *tracker.JobWithTarget {
-	// Defer until "delay" after midnight next day.
-	if time.Since(y.Date) < 24*time.Hour+y.delay {
-		return nil
-	}
-
-	// Copy the jobspec and set the date.
-	jt := &tracker.JobWithTarget{}
-	*jt = y.jobSpecs[y.nextIndex]
-	jt.Job.Date = y.Date
-	jt.ID = jt.Job.Key()
-
-	// Advance to the next jobSpec for next call.
-	y.nextIndex++
-	// When we have dispatched all jobs, advance yesterdayDate to next day
-	// and reset the index.
-	if y.nextIndex >= len(y.jobSpecs) {
-		y.nextIndex = 0
-		y.Date = y.Date.AddDate(0, 0, 1).UTC().Truncate(24 * time.Hour)
-
-		ctx, cf := context.WithTimeout(ctx, 5*time.Second)
-		defer cf()
-		log.Println("Saving", y.GetName(), y.GetKind(), y.Date.Format(timex.YYYYMMDDWithDash))
-		err := y.saver.Save(ctx, y)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-	return jt
-}
-
-func initYesterday(ctx context.Context, saver persistence.Saver, delay time.Duration, specs []tracker.JobWithTarget) (*YesterdaySource, error) {
-	if saver == nil {
-		return nil, ErrNilParameter
-	}
-	// This is the fallback start date.
-	date := tracker.YesterdayDate()
-
-	src := YesterdaySource{
-		saver:     saver,
-		jobSpecs:  specs,
-		Date:      date,
-		delay:     delay,
-		nextIndex: 0,
-	}
-
-	// Recover the date from previously persisted state.
-	ctx, cf := context.WithTimeout(ctx, 5*time.Second)
-	defer cf()
-	err := saver.Fetch(ctx, &src)
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Println("Yesterday starting at", src.Date)
-	return &src, nil
-}
-
-// GetName implements StateObject.GetName
-func (y YesterdaySource) GetName() string {
-	return "singleton" // There is only one job service.
-}
-
-// GetKind implements StateObject.GetKind
-func (y YesterdaySource) GetKind() string {
-	return reflect.TypeOf(y).String()
-}
+// ErrNoConfiguredJobs is returned when a new Job Service cannot be created
+// because there are no configured jobs.
+var ErrNoConfiguredJobs = errors.New("no configured jobs")
 
 // Service contains all information needed to provide a job service.
 // It iterates through successive dates, processing that date from
 // all TypeSources in the source bucket.
 type Service struct {
-	jobSpecs []tracker.JobWithTarget // The job prefixes to be iterated through.
-
-	saver persistence.Saver // This injected to implement persistence.
-
-	startDate time.Time // The date to restart at.
-
-	// Optional jobAdder to add jobs to, e.g. a Tracker
-	jobAdder jobAdder
-
 	// Storage client used to get source lists.
 	sClient stiface.Client
 
@@ -131,23 +32,8 @@ type Service struct {
 	// All fields below are protected by *lock*
 	lock *sync.Mutex
 
-	// The Date is exported for persistence.  It is the only field
-	// that is recovered after restart.  All others are injected
-	// from config.
-	Date      time.Time // The date currently being dispatched.
-	nextIndex int       // index of TypeSource to dispatch next.
-
-	yesterday *YesterdaySource // Provides jobs for high priority yesterday
-}
-
-func (svc *Service) advanceDate() {
-	date := svc.Date.UTC().AddDate(0, 0, 1).Truncate(24 * time.Hour)
-	// Start over when we reach yesterday.
-	if time.Since(date) < 36*time.Hour {
-		date = svc.startDate
-	}
-	svc.Date = date
-	svc.nextIndex = 0
+	dailyJobs *JobIterator // for daily jobs.
+	histJobs  *JobIterator // for historical jobs.
 }
 
 // NextJob returns a tracker.Job to dispatch.
@@ -156,35 +42,18 @@ func (svc *Service) NextJob(ctx context.Context) *tracker.JobWithTarget {
 	defer svc.lock.Unlock()
 
 	// Check whether there is yesterday work to do.
-	if jt := svc.yesterday.nextJob(ctx); jt != nil {
+	if jt, _ := svc.dailyJobs.Next(); jt != nil {
 		log.Println("Yesterday job:", jt.Job)
 		return svc.ifHasFiles(ctx, jt)
 	}
 
-	// Since some jobs may be configured as dailyOnly, or have no files
-	// for a given date, we check for these conditions, and skip the job if appropriate.
-	// If first job isn't suitable, we check up to len(jobSpecs) to try to find one.
-	for attempts := 0; attempts < len(svc.jobSpecs); attempts++ {
-		jt := &tracker.JobWithTarget{}
-		*jt = svc.jobSpecs[svc.nextIndex]
-		jt.Job.Date = svc.Date
-		jt.ID = jt.Job.Key()
-		svc.nextIndex++
-
-		if svc.nextIndex >= len(svc.jobSpecs) {
-			svc.advanceDate()
-			// Note that this will block other calls to NextJob
-			ctx, cf := context.WithTimeout(ctx, 5*time.Second)
-			defer cf()
-			log.Println("Saving", svc.GetName(), svc.GetKind(), svc.Date.Format(timex.YYYYMMDDWithDash))
-			err := svc.saver.Save(ctx, svc)
-			if err != nil {
-				log.Println(err)
-			}
-		}
-
-		// Skip the job if it is a dailyOnly job.
-		if jt.DailyOnly {
+	// Since some jobs may be configured as dailyOnly, or have no files for a
+	// given date, we check for these conditions, and skip the job if
+	// appropriate. We try up to histJobs.Len() times to find the next job.
+	for attempts := 0; attempts < svc.histJobs.Len(); attempts++ {
+		jt, err := svc.histJobs.Next()
+		if err != nil {
+			log.Println(err)
 			continue
 		}
 		return svc.ifHasFiles(ctx, jt)
@@ -206,45 +75,23 @@ func (svc *Service) ifHasFiles(ctx context.Context, jt *tracker.JobWithTarget) *
 	return jt
 }
 
-// Recover the processing date.
-// Not thread-safe - should be called before activating service.
-func (svc *Service) recoverDate(ctx context.Context) {
-	// Default to tracker.LastJob date.
-	// Deprecated
-	svc.Date = svc.jobAdder.LastJob().Date
-
-	// try to recover date from saver.
-	ctx, cf := context.WithTimeout(ctx, 5*time.Second)
-	defer cf()
-	err := svc.saver.Fetch(ctx, svc)
-	if err != nil {
-		log.Println(err, svc)
-	}
-
-	// Adjust if Date is too early.
-	if svc.Date.Before(svc.startDate) {
-		svc.Date = svc.startDate
-	}
-}
-
 // ErrInvalidStartDate is returned if startDate is time.Time{}
 var ErrInvalidStartDate = errors.New("invalid start date")
 
 // NewJobService creates the default job service.
-func NewJobService(ctx context.Context, tk jobAdder, startDate time.Time,
-	targetBase string, sources []config.SourceConfig,
-	saver persistence.Saver,
+func NewJobService(startDate time.Time,
+	sources []config.SourceConfig,
 	statsClient stiface.Client, // May be nil
+	dailySaver namedSaver,
+	histSaver namedSaver,
 ) (*Service, error) {
 	if startDate.Equal(time.Time{}) {
 		return nil, ErrInvalidStartDate
 	}
-	if tk == nil || ctx == nil || saver == nil {
-		return nil, ErrNilParameter
-	}
 
 	// The service cycles through the jobSpecs.  Each spec is a job (bucket/exp/type) and a target GCS bucket or BQ table.
-	specs := make([]tracker.JobWithTarget, 0)
+	dailySpecs := make([]tracker.JobWithTarget, 0)
+	histSpecs := make([]tracker.JobWithTarget, 0)
 	for _, s := range sources {
 		log.Println(s)
 		job := tracker.Job{
@@ -260,41 +107,28 @@ func NewJobService(ctx context.Context, tk jobAdder, startDate time.Time,
 			Job:       job,
 			DailyOnly: s.DailyOnly,
 		}
-		specs = append(specs, jt)
+		dailySpecs = append(dailySpecs, jt)
+		if !s.DailyOnly {
+			histSpecs = append(histSpecs, jt)
+		}
 	}
-	if len(specs) < 1 {
-		log.Fatal("No jobs specified")
+	if len(dailySpecs) == 0 {
+		// NOTE: the set of historical specs may be empty.
+		return nil, ErrNoConfiguredJobs
 	}
 
-	yesterday, err := initYesterday(ctx, saver, 10*time.Hour+30*time.Minute, specs)
-	if err != nil {
-		return nil, err
-	}
+	di := NewDailyIterator(10*time.Hour+30*time.Minute, dailySaver)
+	hi := NewHistoricalIterator(startDate, histSaver)
+
+	dailyJobs := NewJobIterator(di, dailySpecs)
+	histJobs := NewJobIterator(hi, histSpecs)
 
 	svc := Service{
-		jobAdder:  tk,
-		saver:     saver,
-		jobSpecs:  specs,
-		startDate: startDate,
+		dailyJobs: dailyJobs,
+		histJobs:  histJobs,
 		sClient:   statsClient,
 		lock:      &sync.Mutex{},
-		nextIndex: 0,
-		yesterday: yesterday,
 	}
 
-	svc.recoverDate(ctx)
-
 	return &svc, nil
-}
-
-// ---------- Implement persistence.StateObject -------------
-
-// GetName implements StateObject.GetName
-func (svc Service) GetName() string {
-	return "singleton" // There is only one job service.
-}
-
-// GetKind implements StateObject.GetKind
-func (svc Service) GetKind() string {
-	return reflect.TypeOf(svc).String()
 }
--- job-service/job-service_test.go
+++ job-service/job-service_test.go
@@ -3,339 +3,186 @@ package job_test
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"log"
-	"os"
+	"path"
 	"testing"
 	"time"
 
-	"bou.ke/monkey"
-	"github.com/go-test/deep"
-
-	"github.com/m-lab/go/testingx"
-
+	"cloud.google.com/go/storage"
+	"github.com/googleapis/google-cloud-go-testing/storage/stiface"
 	"github.com/m-lab/etl-gardener/config"
 	"github.com/m-lab/etl-gardener/job-service"
 	"github.com/m-lab/etl-gardener/persistence"
 	"github.com/m-lab/etl-gardener/tracker"
-	"github.com/m-lab/etl-gardener/tracker/jobtest"
+	"github.com/m-lab/go/cloudtest/gcsfake"
 )
 
-func init() {
-	// Always prepend the filename and line number.
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+type namedSaver interface {
+	Save(v any) error
+	Load(v any) error
 }
 
-func must(t *testing.T, err error) {
-	if err != nil {
-		log.Output(2, err.Error())
-		t.Fatal(err)
+func TestNewJobService(t *testing.T) {
+	tests := []struct {
+		name        string
+		startDate   time.Time
+		sources     []config.SourceConfig
+		statsClient stiface.Client
+		dailySaver  namedSaver
+		histSaver   namedSaver
+		want        tracker.Key
+		wantErr     error
+	}{
+		{
+			name:      "successful-init",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources: []config.SourceConfig{
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo"},
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "pcap", DailyOnly: true},
+			},
+			dailySaver: &noopSaver{},
+			histSaver:  &noopSaver{},
+			want:       "fake-bucket/ndt/ndt5/20220701",
+		},
+		{
+			name:      "error-bad-start-time",
+			startDate: time.Time{},
+			wantErr:   job.ErrInvalidStartDate,
+		},
+		{
+			name:      "error-no-configured-jobs",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources:   []config.SourceConfig{},
+			wantErr:   job.ErrNoConfiguredJobs,
+		},
 	}
-}
-
-type NullTracker struct{}
-
-func (nt *NullTracker) AddJob(job tracker.Job) error {
-	return nil
-}
-
-func (nt *NullTracker) LastJob() tracker.Job {
-	return tracker.Job{}
-}
-
-type NullSaver struct {
-}
-
-func (s *NullSaver) Save(ctx context.Context, o persistence.StateObject) error {
-	return errors.New("Null Saver")
-}
-func (s *NullSaver) Delete(ctx context.Context, o persistence.StateObject) error {
-	return errors.New("Null Saver")
-}
-func (s *NullSaver) Fetch(ctx context.Context, o persistence.StateObject) error {
-	return errors.New("Null Saver")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := job.NewJobService(tt.startDate, tt.sources, tt.statsClient, tt.dailySaver, tt.histSaver)
+			if tt.wantErr != err {
+				t.Errorf("NewJobService() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr != nil {
+				return
+			}
+			// Attempt to read one job.
+			jt := got.NextJob(context.Background())
+			if jt == nil {
+				t.Errorf("NewJobService().NextJob nil job; got nil, want job")
+				return
+			}
+			if jt.ID != tt.want {
+				t.Errorf("NewJobService().NextJob wrong ID; got %q, want %q", jt.ID, tt.want)
+			}
+		})
+	}
 }
 
 func TestService_NextJob(t *testing.T) {
-	// This allows predictable behavior from time.Since in the advanceDate function.
-	// It will cause wrapping when the service would advance from 2011/2/4 to
-	// 2011/2/5, since 2/5 is less than 36 hours prior to "now"
-	now := time.Date(2011, 2, 6, 1, 2, 3, 4, time.UTC)
-	monkey.Patch(time.Now, func() time.Time {
-		return now
-	})
-	defer monkey.Unpatch(time.Now)
+	// Setup fake gcs bucket access.
+	fakeb := gcsfake.NewBucketHandle()
+	fakeb.ObjAttrs = append(fakeb.ObjAttrs, &storage.ObjectAttrs{Name: "ndt/ndt5/2022/07/01/foo.tgz", Size: 1, Updated: time.Now()})
+	fakec := &gcsfake.GCSClient{}
+	fakec.AddTestBucket("fake-bucket", fakeb)
 
-	ctx := context.Background()
+	// Setup fake daily checkpoint.
+	faked := &job.DailyIterator{Date: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC)}
+	ps := persistence.NewLocalNamedSaver(path.Join(t.TempDir(), "fake-daily.json"))
+	ps.Save(faked)
 
-	sources := []config.SourceConfig{
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo"},
-	}
-	// This is three days before "now".  The job service should restart
-	// when it reaches 36 hours before "now", which is 2011-02-05
-	start := time.Date(2011, 2, 3, 0, 0, 0, 0, time.UTC)
-	svc, err := job.NewJobService(ctx, &NullTracker{}, start, "fake-bucket", sources, &NullSaver{}, nil)
-	must(t, err)
-
-	expected := []struct {
-		body string
+	tests := []struct {
+		name        string
+		startDate   time.Time
+		sources     []config.SourceConfig
+		statsClient stiface.Client
+		dailySaver  namedSaver
+		histSaver   namedSaver
+		want        tracker.Key
+		wantErr     bool
+		cancelCtx   bool
 	}{
-		{body: `{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-03T00:00:00Z"}`},
-		{body: `{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"tcpinfo","Date":"2011-02-03T00:00:00Z"}`},
-		{body: `{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-04T00:00:00Z"}`},
-		{body: `{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"tcpinfo","Date":"2011-02-04T00:00:00Z"}`},
-		// Wrap
-		{body: `{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-03T00:00:00Z"}`},
+		{
+			name:      "success-historical",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources: []config.SourceConfig{
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
+			},
+			statsClient: fakec,
+			dailySaver:  &noopSaver{},
+			histSaver:   &noopSaver{},
+			want:        "fake-bucket/ndt/ndt5/20220701",
+		},
+		{
+			name:      "success-daily",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources: []config.SourceConfig{
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "pcap", DailyOnly: true},
+			},
+			dailySaver: ps, // use persisted and restored date.
+			histSaver:  &noopSaver{},
+			want:       "fake-bucket/ndt/pcap/20220701",
+		},
+		{
+			name:      "error-fail-savers",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources: []config.SourceConfig{
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
+			},
+			dailySaver: &failSaver{err: errors.New("fail")},
+			histSaver:  &failSaver{err: errors.New("fail")},
+			wantErr:    true,
+		},
+		{
+			name:      "error-listing-pcap",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources: []config.SourceConfig{
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "pcap"},
+			},
+			statsClient: fakec, // there are no pcap files in fakec client.
+			dailySaver:  &noopSaver{},
+			histSaver:   &noopSaver{},
+			wantErr:     true,
+		},
+		{
+			name:      "error-cancelled-context",
+			startDate: time.Date(2022, time.July, 1, 0, 0, 0, 0, time.UTC),
+			sources: []config.SourceConfig{
+				{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "pcap"},
+			},
+			statsClient: fakec, // HasFiles will fail b/c the context will be cancelled.
+			dailySaver:  &noopSaver{},
+			histSaver:   &noopSaver{},
+			wantErr:     true,
+			cancelCtx:   true,
+		},
 	}
 
-	for i, e := range expected {
-		want := tracker.Job{}
-		json.Unmarshal([]byte(e.body), &want)
-		got := svc.NextJob(ctx)
-		log.Println(got)
-		diff := deep.Equal(want, got.Job)
-		if diff != nil {
-			t.Error(i, diff)
-		}
-	}
-}
-
-func TestResume(t *testing.T) {
-	// Fake time will avoid yesterday trigger.
-	now := time.Date(2011, 2, 16, 1, 2, 3, 4, time.UTC)
-	monkey.Patch(time.Now, func() time.Time {
-		return now
-	})
-	defer monkey.Unpatch(time.Now)
-
-	ctx := context.Background()
-
-	start := time.Date(2011, 2, 3, 0, 0, 0, 0, time.UTC)
-	saver := tracker.NewLocalSaver(t.TempDir())
-	tk, err := tracker.InitTracker(context.Background(), saver, 0, 0, 0) // Only using jobmap.
-	if err != nil {
-		t.Fatal(err)
-	}
-	lastJobDate := start.AddDate(0, 0, 3)
-	last := jobtest.NewJob("fake-bucket", "ndt", "ndt5", lastJobDate)
-	tk.AddJob(last)
-
-	sources := []config.SourceConfig{
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo"},
-	}
-	svc, err := job.NewJobService(ctx, tk, start, "fake-bucket", sources, &NullSaver{}, nil)
-	must(t, err)
-	j := svc.NextJob(ctx)
-	if j.Job.Date != last.Date {
-		t.Errorf("NextJob() Job date does not match; got %s, want %s", j.Job.Date, last.Date)
-	}
-}
-
-// Implements persistence.Saver, for test injection.
-type FakeSaver struct {
-	Current   time.Time
-	Yesterday time.Time
-}
-
-func (fs *FakeSaver) Save(ctx context.Context, o persistence.StateObject) error {
-	switch svc := o.(type) {
-	case *job.Service:
-		fs.Current = svc.Date
-	case *job.YesterdaySource:
-		fs.Yesterday = svc.Date
-	default:
-		log.Fatal("Not implemented")
-	}
-	return nil
-}
-func (fs *FakeSaver) Delete(ctx context.Context, o persistence.StateObject) error {
-	return nil
-}
-func (fs *FakeSaver) Fetch(ctx context.Context, o persistence.StateObject) error {
-	switch to := o.(type) {
-	case *job.Service:
-		to.Date = fs.Current
-	case *job.YesterdaySource:
-		to.Date = fs.Yesterday
-	default:
-		log.Fatal("Not implemented")
-	}
-	return nil
-}
-
-func assertStateObject(so persistence.StateObject) {
-	assertStateObject(&job.Service{})
-}
-
-func TestResumeFromSaver(t *testing.T) {
-	ctx := context.Background()
-
-	// This allows predictable behavior from time.Since in the advanceDate function.
-	monkey.Patch(time.Now, func() time.Time {
-		return time.Date(2011, 2, 16, 1, 2, 3, 4, time.UTC)
-	})
-
-	start := time.Date(2011, 2, 3, 0, 0, 0, 0, time.UTC)
-	sources := []config.SourceConfig{
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo"},
-	}
-
-	// Set up fake saver.
-	resume := time.Date(2011, 2, 10, 0, 0, 0, 0, time.UTC)
-	// yesterday is set to now, so it won't trigger.
-	yesterday := time.Now().UTC().Truncate(24 * time.Hour)
-	fs := FakeSaver{Current: resume, Yesterday: yesterday}
-	svc, err := job.NewJobService(ctx, &NullTracker{}, start, "fake-bucket", sources, &fs, nil)
-	must(t, err)
-	// NextJob should return a job with date provided by FakeSaver.
-	j := svc.NextJob(ctx)
-	if j.Job.Date != resume {
-		t.Error("Expected ", resume, "got", j)
-	}
-
-	// When NextJob returns the last job for the date, it should trigger
-	// saving the next date to process, which is 20110214.
-	j = svc.NextJob(ctx)
-	// Check that we see the new date in the FakeSaver
-	if fs.Current != resume.AddDate(0, 0, 1) {
-		t.Error("Expected", resume.AddDate(0, 0, 1), "got", fs.Current)
-	}
-}
-
-func TestYesterdayFromSaver(t *testing.T) {
-	ctx := context.Background()
-
-	// This allows predictable behavior from time.Since in the advanceDate function.
-	monkey.Patch(time.Now, func() time.Time {
-		// NOTE: must be later than the default "yesterday" delay.
-		return time.Date(2011, 2, 16, 11, 2, 3, 4, time.UTC)
-	})
-
-	start := time.Date(2011, 2, 3, 0, 0, 0, 0, time.UTC)
-	sources := []config.SourceConfig{
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo"},
-	}
-
-	// Set up fake saver.
-	resume := time.Date(2011, 2, 10, 0, 0, 0, 0, time.UTC)
-	// Set up yesterday so that it triggers immediately.
-	yesterday := time.Now().UTC().Truncate(24*time.Hour).AddDate(0, 0, -2)
-	fs := FakeSaver{Current: resume, Yesterday: yesterday}
-	svc, err := job.NewJobService(ctx, &NullTracker{}, start, "fake-bucket", sources, &fs, nil)
-	must(t, err)
-
-	expected := []struct {
-		body string
-	}{
-		// Yesterday (twice to catch up)
-		{body: `{"ID":"fake-bucket/ndt/ndt5/20110214","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-14T00:00:00Z"}}`},
-		{body: `{"ID":"fake-bucket/ndt/tcpinfo/20110214","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"tcpinfo","Date":"2011-02-14T00:00:00Z"}}`},
-		{body: `{"ID":"fake-bucket/ndt/ndt5/20110215","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-15T00:00:00Z"}}`},
-		{body: `{"ID":"fake-bucket/ndt/tcpinfo/20110215","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"tcpinfo","Date":"2011-02-15T00:00:00Z"}}`},
-		// Resume
-		{body: `{"ID":"fake-bucket/ndt/ndt5/20110210","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-10T00:00:00Z"}}`},
-		{body: `{"ID":"fake-bucket/ndt/tcpinfo/20110210","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"tcpinfo","Date":"2011-02-10T00:00:00Z"}}`},
-		{body: `{"ID":"fake-bucket/ndt/ndt5/20110211","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"ndt5","Date":"2011-02-11T00:00:00Z"}}`},
-		{body: `{"ID":"fake-bucket/ndt/tcpinfo/20110211","Job":{"Bucket":"fake-bucket","Experiment":"ndt","Datatype":"tcpinfo","Date":"2011-02-11T00:00:00Z"}}`},
-	}
-
-	for i, e := range expected {
-		want := &tracker.JobWithTarget{}
-		json.Unmarshal([]byte(e.body), want)
-		got := svc.NextJob(ctx)
-		diff := deep.Equal(want, got)
-		if diff != nil {
-			t.Error(i, diff)
-		}
-	}
-
-	// Check that we see the new date in the FakeSaver
-	if fs.Yesterday != yesterday.AddDate(0, 0, 2) {
-		t.Error("Expected", yesterday.AddDate(0, 0, 1), "got", fs.Yesterday)
-	}
-}
-
-func TestEarlyWrapping(t *testing.T) {
-	ctx := context.Background()
-
-	// This allows predictable behavior from time.Since in the advanceDate function.
-	monkey.Patch(time.Now, func() time.Time {
-		return time.Date(2011, 2, 6, 1, 2, 3, 4, time.UTC)
-	})
-	defer monkey.Unpatch(time.Now)
-	start := time.Date(2011, 2, 3, 0, 0, 0, 0, time.UTC)
-	saver := tracker.NewLocalSaver(t.TempDir())
-	tk, err := tracker.InitTracker(context.Background(), saver, 0, 0, 0) // Only using jobmap.
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	sources := []config.SourceConfig{
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo"},
-		{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "pcap", DailyOnly: true}, // skipped.
-	}
-	svc, err := job.NewJobService(ctx, tk, start, "fake-bucket", sources, &NullSaver{}, nil)
-	must(t, err)
-
-	// If a job is still present in the tracker when it wraps, /job returns an error.
-	expected := []tracker.Job{
-		tracker.Job{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5", Date: start},
-		tracker.Job{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo", Date: start},
-		tracker.Job{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5", Date: start.AddDate(0, 0, 1)},
-		tracker.Job{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "tcpinfo", Date: start.AddDate(0, 0, 1)},
-		// This one should work, because we complete it in the loop.
-		tracker.Job{Bucket: "fake-bucket", Experiment: "ndt", Datatype: "ndt5", Date: start},
-	}
-
-	for k, realJob := range expected {
-		got := svc.NextJob(context.Background())
-		tk.AddJob(got.Job)
-		if k == 2 {
-			// Simulate "monitor" behavior and mark job complete to prevent AddJob error.
-			status, _ := tk.GetStatus(got.Job.Key())
-			status.NewState(tracker.Complete)
-			err := tk.UpdateJob(got.Job.Key(), status)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svc, err := job.NewJobService(tt.startDate, tt.sources, tt.statsClient, tt.dailySaver, tt.histSaver)
 			if err != nil {
-				t.Error(err)
+				t.Errorf("NewJobService() error = %v, want nil", err)
+				return
 			}
-		}
-
-		if got.Job != realJob {
-			t.Errorf("NextJob() wrong job: got %v, want %v", got.Job, realJob)
-		}
-	}
-}
-
-func assertYesterdayStateObject(so persistence.StateObject) {
-	assertYesterdayStateObject(&job.YesterdaySource{})
-}
-
-func TestPersistence(t *testing.T) {
-	ctx := context.Background()
-	ls := persistence.NewLocalSaver(t.TempDir())
-
-	now := time.Now()
-	svc := job.Service{Date: now}
-	err := ls.Save(ctx, &svc)
-	testingx.Must(t, err, "Save error")
-
-	svc.Date = time.Time{}
-	err = ls.Fetch(ctx, &svc)
-	testingx.Must(t, err, "Fetch error")
-	if svc.Date.Unix() != now.Unix() {
-		t.Error("Date should be now", &svc, now.Unix(), svc.Date.Unix())
-	}
-
-	err = ls.Delete(ctx, &svc)
-	testingx.Must(t, err, "Delete error")
-	err = ls.Fetch(ctx, &svc)
-	if !errors.Is(err, os.ErrNotExist) {
-		t.Fatal("Should have errored", err)
+			ctx := context.Background()
+			if tt.cancelCtx {
+				var cancel func()
+				ctx, cancel = context.WithCancel(ctx)
+				cancel() // cancel it.
+			}
+			got := svc.NextJob(ctx)
+			if (got == nil) != tt.wantErr {
+				t.Errorf("NextJob() returned %v, wantErr %t", got, tt.wantErr)
+				return
+			}
+			if got == nil {
+				return
+			}
+			if got.ID != tt.want {
+				t.Errorf("Service.NextJob() wrong job; got %v, want %v", got.ID, tt.want)
+			}
+		})
 	}
 }
