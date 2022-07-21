f9f9e60838b0121c4ebcb1aa247782faeb6f00cc
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-07-21 17:32:12 -0400

    Remove obsolete LastJob method

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -439,8 +439,3 @@ func (tr *Tracker) WriteHTMLStatusTo(ctx context.Context, w io.Writer) error {
 
 	return jobs.WriteHTML(w)
 }
-
-// LastJob returns the last Job successfully added with AddJob
-func (tr *Tracker) LastJob() Job {
-	return tr.lastJob
-}
