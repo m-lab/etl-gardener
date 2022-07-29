19ec9eb99e1b4ba561f40db08cff12addd5bd9de
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-07-29 09:30:50 -0400

    Remove more obsolete v1 logic (#405)
    * Remove v1 sanity methods & notes
    * Remove v1 client
    * Remove obsolete LocalSaver

--- persistence/persistence.go
+++ persistence/persistence.go
@@ -1,96 +1,10 @@
 package persistence
 
 import (
-	"context"
 	"encoding/json"
-	"io/ioutil"
 	"os"
-	"path"
 	"sync"
 )
-
-// StateObject defines the interface for objects to be saved/retrieved from persistent storage.
-type StateObject interface {
-	GetName() string
-	// Should be implemented in the actual type, as
-	// func (o ConcreteType) GetKind() string {
-	//   return reflect.TypeOf(o).String()
-	// }
-	GetKind() string
-}
-
-// Base is the base for persistent objects.  All StateObjects should embed
-// Base and call NewBase to initialize.
-type Base struct {
-	Name string
-}
-
-// GetName implements StateObject.Name
-func (o Base) GetName() string {
-	return o.Name
-}
-
-// NewBase initializes a Base object.
-func NewBase(name string) Base {
-	return Base{Name: name}
-}
-
-// Saver provides API for saving and retrieving StateObjects.
-type Saver interface {
-	Save(ctx context.Context, o StateObject) error
-	Delete(ctx context.Context, o StateObject) error
-	Fetch(ctx context.Context, o StateObject) error
-}
-
-// LocalSaver implements a Saver that stores state objects in local files.
-type LocalSaver struct {
-	Namespace string
-	dir       string
-	lock      sync.Mutex
-}
-
-// NewLocalSaver creates and returns a new local saver.
-func NewLocalSaver(dir string) *LocalSaver {
-	return &LocalSaver{dir: dir, Namespace: "gardener"}
-}
-
-func (ls *LocalSaver) fname(o StateObject) string {
-	return path.Join(ls.dir, ls.Namespace+"-"+o.GetKind()+"-"+o.GetName())
-}
-
-// Save implements Saver.Save using local files.
-func (ls *LocalSaver) Save(ctx context.Context, o StateObject) error {
-	ls.lock.Lock()
-	defer ls.lock.Unlock()
-	f, err := os.OpenFile(ls.fname(o), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
-	b, err := json.Marshal(o)
-	if err != nil {
-		return err
-	}
-	_, err = f.Write(b)
-	return err
-}
-
-// Delete implements Saver.Delete using local files.
-func (ls *LocalSaver) Delete(ctx context.Context, o StateObject) error {
-	ls.lock.Lock()
-	defer ls.lock.Unlock()
-	return os.Remove(ls.fname(o))
-}
-
-// Fetch implements Saver.Fetch to fetch state of requested StateObject from local files.
-func (ls *LocalSaver) Fetch(ctx context.Context, o StateObject) error {
-	ls.lock.Lock()
-	defer ls.lock.Unlock()
-	b, err := ioutil.ReadFile(ls.fname(o))
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(b, o)
-}
 
 // LocalNamedSaver is a generic saver interface for persisting to/from JSON
 // objects.
--- persistence/persistence_test.go
+++ persistence/persistence_test.go
@@ -1,7 +1,6 @@
 package persistence_test
 
 import (
-	"context"
 	"log"
 	"os"
 	"path"
@@ -14,59 +13,6 @@ import (
 func init() {
 	// Always prepend the filename and line number.
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-}
-
-type O1 struct {
-	persistence.Base
-
-	Integer int32
-}
-
-// Kind implements StateObject.Kind
-func (o O1) GetKind() string {
-	return reflect.TypeOf(o).Name()
-}
-
-func NewO1(name string) O1 {
-	return O1{Base: persistence.NewBase(name)}
-}
-
-func assertStateObject(so persistence.StateObject) {
-	assertStateObject(O1{})
-}
-
-func TestNewLocalSaver(t *testing.T) {
-	t.Run("create-save-fetch-delete", func(t *testing.T) {
-		dir := t.TempDir()
-		got := persistence.NewLocalSaver(dir)
-		if got == nil {
-			t.Fatalf("NewLocalSaver() = nil, want valid local saver.")
-		}
-		o := NewO1("foo-state")
-		ctx := context.Background()
-		o.Integer = 101 // Value to persist.
-
-		err := got.Save(ctx, o)
-		if err != nil {
-			t.Errorf("LocalSaver.Save() = %v, want nil", err)
-		}
-		o2 := NewO1("foo-state")  // Without value.
-		err = got.Fetch(ctx, &o2) // Read value.
-		if err != nil {
-			t.Errorf("LocalSaver.Fetch() = %v, want nil", err)
-		}
-		if o != o2 { // Compare values.
-			t.Errorf("LocalSaver.Fetch() wrong result; got %v, want %v", o2, o)
-		}
-		err = got.Delete(ctx, o)
-		if err != nil {
-			t.Errorf("LocalSaver.Delete() = %v, want nil", err)
-		}
-		err = got.Fetch(ctx, &o2) // Read value after delete.
-		if err == nil {
-			t.Errorf("LocalSaver.Fetch() returned value after delete; got nil, want err")
-		}
-	})
 }
 
 func TestLocalNamedSaver(t *testing.T) {
