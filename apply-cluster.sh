7ead3a886591f1b4e16fc2c62520b9c607aa7219
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-12-01 11:40:31 -0500

    Process annotation & ndt from same source in staging for testing

--- apply-cluster.sh
+++ apply-cluster.sh
@@ -18,13 +18,12 @@ CLUSTER=${CLOUDSDK_CONTAINER_CLUSTER:?Please provide cluster name: $USAGE}
 DATE_SKIP=${DATE_SKIP:-"0"}  # Number of dates to skip between each processed date (for sandbox).
 TASK_FILE_SKIP=${TASK_FILE_SKIP:-"0"}  # Number of files to skip between each processed file (for sandbox).
 
-# Use sandbox in sandbox, staging in staging, and use measurement-lab in oti.
+# Use sandbox in sandbox, measurement-lab in staging and oti.
 SOURCE_PROJECT=${PROJECT_ID/mlab-oti/measurement-lab}
+SOURCE_PROJECT=${SOURCE_PROJECT/mlab-staging/measurement-lab}
 sed -i \
     -e 's/{{ANNOTATION_SOURCE_PROJECT}}/'${SOURCE_PROJECT}'/g' \
     config/config.yml
-# Use sandbox in sandbox, measurement-lab in staging and oti.
-SOURCE_PROJECT=${SOURCE_PROJECT/mlab-staging/measurement-lab}
 sed -i \
     -e 's/{{NDT_SOURCE_PROJECT}}/'${SOURCE_PROJECT}'/g' \
     config/config.yml
